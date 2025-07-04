bad9b5f9403530f3f95df4e6accb06c3d2b05305
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-04 15:08:57 +0530

    Javascript chapter 1 fully done

--- week1/JS practice/chapter1/practice_question/README.md
+++ week1/JS practice/chapter1/practice_question/README.md
@@ -2,3 +2,6 @@
 
 1. Create a const object called "product" to store information shown in the picture below:
 ![alt text](image.png)
+
+2. Create a const object called "profile" to store information storing information in the picture below:
+![alt text](image-1.png)
---
+++ week1/JS practice/chapter1/practice_question/code.js
@@ -0,0 +1,22 @@
+const product = {
+    penName: "Parker Jotter Standrard CT Ball Pen",
+    color: "black",
+    rating: 5,
+    numberOfReviews: 7002,
+    dealOfTheDat: true,
+    cost: 270,
+    mrp: 285,
+    discount: 5,
+};
+
+const profile = {
+    username: "@shradhakapra",
+    isFollow: true,
+    message: false,
+    numberOfPosts: 195,
+    numberOfFollowers: 569000,
+    numberOfFollowing: 4,
+    fullName: "Shradha Kapra",
+    job: "Entrepreneurship",
+    bio: "Apna College | Ex-Microsoft, DRDO \n To educate someone is the highest privilege <3",
+};
