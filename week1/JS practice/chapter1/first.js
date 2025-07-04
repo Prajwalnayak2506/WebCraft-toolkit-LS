7a99528a1eb60ff3cd61b8896ad4d08aae4b6e77
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-04 13:21:01 +0530

    Javascript chapter 1 ongoing

---
+++ week1/JS practice/chapter1/first.js
@@ -0,0 +1,28 @@
+// This code is for printing soemthing
+console.log("Prajwal is amazing");
+console.log("prajwal learning JS");
+// This is VS Code, but how to make it seee on browser?????
+// for that we need a html file
+
+// defining variables
+name = "Prajwal Nayak"; //string
+age = 21; // number value
+console.log(name, "is", age, "years old."); //printing
+x = null; //null type
+isFollow  = false; //boolean
+
+
+//let, var, const
+var num = 14; //all allowed; global score
+let numm = 13; // no redeclare but can update; local scope
+const coools = 15; //on redeclare no update' local scope
+
+// datatype
+console.log(typeof name); //shows as string
+console.log(typeof x); //shows data type as object but is considered primitive
+let y = BigInt("123");
+let z = Symbol("Helllooo!!")
+console.log("this is", y,"and its data type is", typeof y); //shows as string
+console.log("this is", z,"and its data type is", typeof z); //shows as string
+
+
