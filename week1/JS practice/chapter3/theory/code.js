0e87d04a8fd7cb536097babcecd2bb32c3fe0544
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-07 13:38:11 +0530

    chapter 3 started

---
+++ week1/JS practice/chapter3/theory/code.js
@@ -0,0 +1,47 @@
+//while loop
+for(let i=1;i<=5;i++){
+    console.log("Hi");
+}
+
+//calculate sum of frst 5 numbers
+let sum=0;
+for (let i =1;i<6;i++){
+    sum+=i;
+}
+console.log(sum);
+
+//print i 5 times -->block vs gobal scope
+for (let i=1; i<=5;i++){
+    console.log("i=",i);
+}
+// console.log(i); gives an error since i is let variable so it has gloal scope but if i has var type, then it would not have an error since i is given global scope and then i would be print as 6
+//explained infintie loop
+//while loop
+let j=1;
+while(j<6){
+    console.log("j=",j);
+    j++;
+}
+let k=0;
+//do while loop
+do{
+    console.log("k=",k);
+    k++;
+}while(k===1);
+
+//for-of
+let str = "google";
+for(let p of str){
+    console.log(p);
+}
+
+//for-in
+let student={
+    name: "rahul",
+    age: 20,
+    cgpa: 7.54,
+    isPass: true,
+};
+for(let key in student){
+    console.log("key=",key,"   value=",student[key]); //prints key
+}
