df4a9b14caa30ab9f69f8ae03dcbacab0933485a
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-07 14:42:29 +0530

    chapter 3 done practice questions

--- week1/JS practice/chapter3/practice_questions/README.md
+++ week1/JS practice/chapter3/practice_questions/README.md
@@ -1,3 +1,3 @@
 the practice questions are:
 1. print all even numbers from 0 to 100
-2. 
+2. create a number where u set a random value to a variable and ask the user to guess a number and ask the user to keep guessing until the right number is guessed
--- week1/JS practice/chapter3/practice_questions/code.js
+++ week1/JS practice/chapter3/practice_questions/code.js
@@ -1,3 +1,4 @@
+// question 1
 let n = 100;
 console.log("1. The even number from 1 to 100 are:")
 for(let i=1;i<=n;i++){
@@ -5,3 +6,11 @@ for(let i=1;i<=n;i++){
         console.log(i);
     }
 }
+
+//question 2
+let game_number = 25;
+guess = prompt("Guess the number");
+while(game_number != guess){
+    guess = prompt("Guess the number");
+}
+alert("You guessed the right number");
