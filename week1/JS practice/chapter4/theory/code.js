22f42303d51da75bce943b35c68342764d417688
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-08 21:05:28 +0530

    theory partyl done and startign with pratice question 1

--- week1/JS practice/chapter4/theory/code.js
+++ week1/JS practice/chapter4/theory/code.js
@@ -1,1 +1,20 @@
 //arrays
+let marks =[12,45,65,43,22 ]; //is an object
+console.log(marks.length);
+console.log(typeof marks);
+//array indices
+console.log(marks[2]);
+console.log(marks[9]);
+//strings are immutable but arrays are mutable
+marks[1] = 0;
+console.log(marks);
+//printing all elements in an arrray
+for(let idx=0;idx<marks.length;idx++){
+    console.log(marks[idx]);
+}
+for(let el of marks){
+    console.log(el);
+}
+for(let el in marks){
+    console.log(marks[el]);
+}
