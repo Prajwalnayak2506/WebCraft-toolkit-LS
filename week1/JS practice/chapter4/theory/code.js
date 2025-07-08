b3a4949d78bd3cc968035cffaf8410b2e4ad2488
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-08 21:48:19 +0530

    theory partyl done and startign with pratice question 3

--- week1/JS practice/chapter4/theory/code.js
+++ week1/JS practice/chapter4/theory/code.js
@@ -18,3 +18,26 @@ for(let el of marks){
 for(let el in marks){
     console.log(marks[el]);
 }
+//methods in array
+let veggies =["potato","tomato","onion","spinach"];
+veggies.push("sprouts","beetroot");//push
+console.log(veggies);
+let deletedItem = veggies.pop();//returns the delted value in the array
+console.log(veggies);
+console.log(`The deleted item is ${deletedItem}`);
+console.log(veggies.toString());//to string
+console.log(veggies);
+roots = ["carrot","beetroot","radish"];
+let allVeggies = veggies.concat(roots); //concat
+console.log(allVeggies);
+console.log(veggies);
+console.log(roots);
+let val1 = veggies.shift();//shift
+console.log(`deleted ${val1}`);
+console.log(veggies);
+let val2 = veggies.unshift("potato");//unshift 
+console.log(val2,veggies);
+let slicedArray = veggies.slice(1,3);//slice
+console.log(veggies,slicedArray);
+veggies.splice(2,2,"ginger","garlic","cinnamon");//splice
+console.log(veggies);
