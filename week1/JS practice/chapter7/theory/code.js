e6db5392250e42776276056f5796bcf5f42a6d76
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-10 11:50:11 +0530

    chapter 7 theory done

--- week1/JS practice/chapter7/theory/code.js
+++ week1/JS practice/chapter7/theory/code.js
@@ -10,3 +10,29 @@ p.setAttribute("id","box");
 //style
 //changing styles in js
 div.style.backgroundColor = "green";
+
+//inserts elements
+//2 step process : create and then add in required positions
+let newButton = document.createElement("button");
+newButton.innerText = "CLICK ME!";
+let body = document.querySelector("body");
+let n = prompt("Hello guys, ENter 2:");
+if(n == 2){
+    body.append(newButton); //adds button in the end of the body tag
+}
+else if(n==3){
+    body.prepend(newButton); //adds button in starting of the body tag
+}
+else if(n==4){
+    let paraaa = document.querySelectorAll("p");
+    paraaa[1].before(newButton);
+}
+else{
+    let paraaa = document.querySelectorAll("p");
+    paraaa[0].after(newButton);    
+}
+let n1 = prompt("Hello guys, ENter 5:");
+if(n1==5){
+    let paraaa = document.querySelectorAll("p");
+    paraaa[0].remove();
+}
