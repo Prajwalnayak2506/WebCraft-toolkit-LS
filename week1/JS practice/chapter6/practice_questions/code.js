477eaa0aad72a1b0ae8f1ccfcadaf87bbf82479e
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-10 11:13:19 +0530

    chapter 6 practice questions done

--- week1/JS practice/chapter6/practice_questions/code.js
+++ week1/JS practice/chapter6/practice_questions/code.js
@@ -1,3 +1,17 @@
 //question 1
 let h2 = document.querySelector("h2");
 h2.innerText += " fron Apna College students";
+
+//question 2
+let divs = document.querySelectorAll(".box");
+console.dir(divs);
+divs[0].innerText = "UniqueTExt1";
+divs[1].innerText = "UniqueTExt2";
+divs[2].innerText = "UniqueTExt3";
+//better : 
+let idx = 0;
+for(let div of divs){
+    idx++;
+    console.log(div);
+    div.innerText=`UniqueDiv ${idx}`;
+}
