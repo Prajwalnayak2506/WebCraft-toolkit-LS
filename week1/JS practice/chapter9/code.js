ea0577e50e515e7ba97c81e00d33d1d6b54c6374
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-11 09:44:00 +0530

    game done

--- week1/JS practice/chapter9/code.js
+++ week1/JS practice/chapter9/code.js
@@ -1,5 +1,7 @@
 let userScore = 0;
 let compScore = 0;
+let prevCompChoice;
+let gameOver = false;
 let US = document.querySelector("#userScore");
 let CS = document.querySelector("#compScore");
 const choices = document.querySelectorAll(".choice");
@@ -9,12 +11,20 @@ choices.forEach((choice) => {
         playGame(userChoiceId);
     });
 });
+let userWinNum = 0;
 message = "Play Your move";
 const playGame =(userChoice) =>{
+    if(gameOver) return;
     console.log("user choice = ", userChoice);
     //generate computer choice
     let compChoice = genCompChoice();
     console.log("computer choice =",compChoice);
+    let compChoiceId = "#"+compChoice;
+    let compChoiceBack = document.querySelector(compChoiceId);
+    compChoiceBack.classList.add("compChoice");
+    setTimeout(() => {
+        compChoiceBack.classList.remove("compChoice");
+    }, 500);
     if(userChoice === compChoice){
         drawGame()
     }
@@ -44,19 +54,28 @@ const showWinner = (userWin) => {
     }
     if(userScore === 5){
         message = "You Win, Click to play again";
+        userWinNum = 1;
+        gameOver = true;
         restart();
     }
     else if(compScore === 5){
         message = "You Lose, Click to play again";
+        userWinNum = 0;
+        gameOver = true;
         restart();
     }
 }
 const restart = () => {
-    userScore = 0;
-    compScore = 0;
     let msg = document.querySelector("#msg");
     msg.innerText = message ;
-    console.log(message);
+    msg.style.cursor = "pointer";
+    if (userWinNum === 1){
+        msg.style.backgroundColor = "green";
+    }else{
+        msg.style.backgroundColor = "red";
+    }
+    userWinNum = 0;
+    console.log(message);  
 }
 const drawGame =() =>{
     console.log("Draw game");
@@ -66,3 +85,17 @@ const genCompChoice =() => {
     const randIdx = Math.floor(Math.random() * 3)
     return options[randIdx];
 }
+let msg = document.querySelector("#msg");
+msg.addEventListener("click", () => {
+    if (!gameOver) return;
+    // Reset game
+    userScore = 0;
+    compScore = 0;
+    US.innerText = 0;
+    CS.innerText = 0;
+    message = "Play Your move";
+    msg.innerText = message;
+    msg.style.backgroundColor = "blue";
+    msg.style.cursor = "default";
+    gameOver = false;
+});
