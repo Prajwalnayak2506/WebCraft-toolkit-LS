a73a1a1a381948ca4dfc6fa88bd612b5bc70dbb7
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-11 09:58:28 +0530

    starting xhapter 10

---
+++ week1/JS practice/chapter10/theory/code.js
@@ -0,0 +1,8 @@
+const student = {
+    name : "prajj", //property of object
+    marks : 45, //proprty of object
+    printMarks: function() {
+        console.log("marks =",this.marks); //method of object
+        //this is a keyword
+    }
+}
