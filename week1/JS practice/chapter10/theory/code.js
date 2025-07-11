691df5100a20a97d8628243cb8259e12a76d1654
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-11 10:12:00 +0530

    continuing chapter 10

--- week1/JS practice/chapter10/theory/code.js
+++ week1/JS practice/chapter10/theory/code.js
@@ -6,3 +6,21 @@ const student = {
         //this is a keyword
     }
 }
+
+//now consider an array
+let arr = [1,2,3,4,5];
+arr.push(3);
+console.log(arr);
+//arr is an object, and we are usign a method push onto it, but it was never deifned
+//soem object have soe predeinfed functions in them which are a part of the prototype object
+// if we create two objects aand y, we can use the functoins of x in y by setting the prototype of y as follows:
+let x = {
+    calcTax(){
+        console.log("tax rate is 10%");
+    },
+};
+const y = {
+    salary: 50000,
+};
+//to use the fucntoin calcTax in y, we can do the following:
+y.__proto__ = x;
