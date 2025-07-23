a68fa4930fe0b66f39cc0e49c2cf305cbbca937d
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-07-23 15:39:47 +0530

    chapter 12 started

---
+++ week1/JS practice/chapter12/theory/code.js
@@ -0,0 +1,12 @@
+//fetch api
+//API is a system which we send requests to,and it sends us an accurate response in the form of some data.
+//it works on request and responses
+//we learn to fetch data form APIs
+//API = Application Programming Interface
+//The fetch AP provides an interface for fetching (sending/receiving) resources.
+//It uses Request and Response objects
+//the fetch() methods is used to fetch a resource (data).
+//free APIs : we will have a lot of APIs : all APIs generally have their own documentation
+//let us choose cat facts https://cat-fact.herokuapp.com
+const URL = "https://cat-fact.herokuapp.com/facts";
+
